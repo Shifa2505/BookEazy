6cf6d3cfdee7587908d97e903c1bad922bd2fa4d
ManavDaggula <manavdaggula@gmail.com>
2023-09-27 21:17:40 +0530

    added user model

--- backend/dbInit.js
+++ backend/dbInit.js
@@ -4,9 +4,10 @@ import { data } from "./dummyData.js";
 import { categoryModel } from "./models/categoryModel.js";
 import { serviceModel } from "./models/serviceModel.js";
 import { servicepersonModel } from "./models/servicepersonModel.js";
+import { userModel } from "./models/userModel.js"
 dotenv.config()
 // will have to later remove this
-const url = process.env.Test_Database_URL
+const url = process.env.Database_URL
 
 const connectDB = async() => {
     try {
@@ -24,8 +25,10 @@ await connectDB();
 await categoryModel.deleteMany();
 await serviceModel.deleteMany();
 await servicepersonModel.deleteMany();
+await userModel.deleteMany();
 await categoryModel.insertMany(data.serviceCategory);
 await serviceModel.insertMany(data.services);
 await servicepersonModel.insertMany(data.servicepersons);
+await userModel.insertMany(data.users);
 
 mongoose.disconnect()
