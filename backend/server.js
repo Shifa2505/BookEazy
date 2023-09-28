bd0cecb0107c2f72940853a3a5ebb4cc8a9ceafb
Shifa25 <shifashaikh2505@gmail.com>
2023-09-28 21:18:50 +0530

    small changes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 // const dotenv = require("dotenv");
+import dotenv from "dotenv";
 import cors from "cors";
+import {connectDB} from "./config/config.js"
 
 const app = express();
 app.use(express.json());
