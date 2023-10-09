5118295c8a558ee858411b00dc2b80b43e8256f7
ManavDaggula <manavdaggula@gmail.com>
2023-10-09 21:45:02 +0530

    login auth in ui

--- bookeazy/src/App.jsx
+++ bookeazy/src/App.jsx
@@ -7,33 +7,39 @@ import PlumbingRepairs from './Components/services/PlumbingRepairs'
 import ElectricalHelp from './Components/services/ElectricalHelp'
 
 import Painting from './Components/services/painting'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useSearchParams } from 'react-router-dom'
 import ServiceMen from './Components/servicemen/ServiceMen'
 import About from './Components/about-page/About'
 import Login from './Components/authentication/login'
 import FinalBooking from './Components/final-booking/FinalBooking'
+import { createContext, useState } from 'react'
+import ShowRequests from './Components/booking-requests/ShowRequests'
+
+export const UserContext = createContext();
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <>
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/services" element={<ShowListOfServicers />} />
-        <Route path="/contact-us" element={<ContactPage />} />
-        <Route path="/service1" element={<PlumbingRepairs/>} /> 
-        <Route path="/service2" element={<ElectricalHelp/>} /> 
-        <Route path="/service3" element={<Painting/>} /> 
-        <Route path="/servicemen" element={<ServiceMen />} />
-        <Route path="/service3" element={<Painting/>} />
-        <Route path="/about" element={<About/>} /> 
-        <Route path="/finalBook" element={<FinalBooking/>} /> 
-        <Route path="/login" element={<Login/>} /> 
-        
-
-        {/* <Route path='/service1' Component={<ElectricalHelp/>}/> */}
-      </Routes>
+      <UserContext.Provider value={{user, setUser}}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/services" element={<ShowListOfServicers />} />
+          <Route path="/contact-us" element={<ContactPage />} />
+          <Route path="/service1" element={<PlumbingRepairs/>} />
+          <Route path="/service2" element={<ElectricalHelp/>} />
+          <Route path="/service3" element={<Painting/>} />
+          <Route path="/servicemen" element={<ServiceMen />} />
+          <Route path="/service3" element={<Painting/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/finalBook" element={<FinalBooking/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/test" element={<ShowRequests/>} />
+          {/* <Route path='/service1' Component={<ElectricalHelp/>}/> */}
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
     </>
   )
--- bookeazy/src/Components/authentication/login.jsx
+++ bookeazy/src/Components/authentication/login.jsx
@@ -1,8 +1,35 @@
 // import React from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
-
+import { UserContext } from "../../App";
+import { useContext, useRef } from "react";
+import axios from "axios";
 function Login(){
+    const {user, setUser} = useContext(UserContext);
+    const usernameRef = useRef(null);
+    const passwordRef = useRef(null);
+    const userTypeRef = useRef(null);
+    
+    function validateAndLogin(){
+        if(usernameRef.current.value.trim().length==0){
+            window.alert("Please enter username")
+            return
+        }
+        if(passwordRef.current.value.trim().length==0){
+            window.alert("Please enter password")
+            return
+        }
+        // console.log(userTypeRef.current.value)
+        if(userTypeRef.current.value=="User"){
+            axios.post("http://localhost:8000/sign-in/user",{username: usernameRef.current.value, password:passwordRef.current.value}, {withCredentials: true})
+            .then(data=>{
+                console.log(data);
+                setUser(data.data)
+            })
+            .catch(err=>console.error(err.response.data))
+        }
+
+    }
     return(
         // <h1>hi shifa login</h1>
         <section className="login-section">
@@ -11,15 +38,19 @@ function Login(){
                     <h1>Login</h1>
                     {/* <h2>User Name</h2> */}
                     <label htmlFor="username">User Name:</label> <br/>
-                    <input type="text" id="username" name="username" placeholder="Enter you User Name" required/><br/>
+                    <input ref={usernameRef} type="text" id="username" name="username" placeholder="Enter you User Name" required/><br/>
                     <label htmlFor="password">Password:</label><br/>
-                    <input type="password" id="password" name="password" placeholder="Enter you Password" required/><br/>
+                    <input ref={passwordRef} type="password" id="password" name="password" placeholder="Enter you Password" required/><br/>
+                    <select ref={userTypeRef} defaultValue={"Client"}>
+                        <option value={"User"}>Client</option>
+                        <option value={"Serviceperson"}>Serviceperson</option>
+                    </select>
                     <p>New here? <br />Sign up today as a{' '}
                         <Link to="/customerSignUp">Customer</Link>{' '}
                         or{' '}
                         <Link to="/serviceProviderSignUp">Service Provider</Link>!
                     </p>
-                    <button className="confirm-btn">Login</button>
+                    <button className="confirm-btn" onClick={validateAndLogin}>Login</button>
                 </div>
                 <div className="right-login">
                     <img src="/images/login.jpg" alt="" width={200} height={200}/>
