e764a70dd319b5e6f3101d5dac4a0c907d073e21
ManavDaggula <manavdaggula@gmail.com>
2024-01-22 13:01:24 +0530

    server test database

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ app.use(cors({credentials: true, origin:"http://localhost:5173"}));
 app.use(cookieParser());
 dotenv.config();
 
-// await connectDB(process.env.Test_Database_URL);
-await connectDB(process.env.Database_URL);
+await connectDB(process.env.Test_Database_URL);
+// await connectDB(process.env.Database_URL);
 
 app.get("/", (req, res) => {
   res.send("Server is active.");
