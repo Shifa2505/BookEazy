08a9374a3159aa007aa56555a940a942a3d9ac26
ManavDaggula <manavdaggula@gmail.com>
2024-02-03 16:42:58 +0530

    started registration page

--- bookeazy/src/Components/authentication/serviceRegister.jsx
+++ bookeazy/src/Components/authentication/serviceRegister.jsx
@@ -9,27 +9,53 @@ export default function Register(){
     const [data, setData] = useState({
         name: '',
         email: '',
-        userType: 'Contributor',
+        phone: '',
+        location: '',
+        username: '',
         password: '',
         
     })
     const registerUser = async (e) => {
         e.preventDefault()
-        const {name, email, userType, password} = data
-        try {
-            const {data} = await axios.post('/register', {
-                name, email, userType, password
-            })
-            if(data.error){
-                toast.error(data.error)
-            }else {
-                setData({})
-                toast.success('Registration Successful. Login to continue!');
-                navigate('/login')
-            }
-        }catch (error){
-            console.log(error)
+        const {name, email, phone, location, username, password} = data;
+        if(!name.trim()){
+            toast.error("Please enter your name.");
+            return;
         }
+        if(!email.trim()){
+            toast.error("Please enter your email.");
+            return;
+        }
+        if(!phone.trim()){
+            toast.error("Please enter your phone.");
+            return;
+        }
+        if(!location.trim()){
+            toast.error("Please enter your location.");
+            return;
+        }
+        if(!username.trim()){
+            toast.error("Please enter your username.");
+            return;
+        }
+        if(!password.trim()){
+            toast.error("Please enter your password.");
+            return;
+        }
+        // try {
+        //     const {data} = await axios.post('/register', {
+        //         name, email, userType, password
+        //     })
+        //     if(data.error){
+        //         toast.error(data.error)
+        //     }else {
+        //         setData({})
+        //         toast.success('Registration Successful. Login to continue!');
+        //         navigate('/login')
+        //     }
+        // }catch (error){
+        //     console.log(error)
+        // }
     }
     return(
         <div>
@@ -42,20 +68,26 @@ export default function Register(){
                             <input type="text" id="username" placeholder="enter name..." value={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
                             <label>Email:</label>
                             <input type="email" id="email" placeholder="enter email..." value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+                            <label>Phone:</label>
+                            <input type="text" id="phone" placeholder="enter phone..." value={data.phone} onChange={(e) => setData({...data, phone: e.target.value})} />
+                            <label>location:</label>
+                            <input type="text" id="location" placeholder="enter location..." value={data.location} onChange={(e) => setData({...data, location: e.target.value})} />
+                            <label>Username:</label>
+                            <input type="text" id="username" placeholder="enter username..." value={data.username} onChange={(e) => setData({...data, username: e.target.value})} />
                             <label>Password:</label>
                             <input type="password" id="password" placeholder="enter password..." value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/>
-                            <label>User Type: </label>
+                            {/* <label>User Type: </label>
                             <select name="userType" id="usertype" value={data.userType} onChange={(e) => setData({ ...data, userType: e.target.value })}> 
                             <option id="usertype" value="Contributor">Contributor</option>
                             <option id="usertype" value="Organization">Organization</option>
-                            </select>
+                            </select> */}
                             <p>Already have an account?<Link className='link' to='/login'>Login</Link></p> 
                             
                             <button className="confirm-btn" type='submit'>Submit</button>
                         </div>
-                        <div className="right-register">
+                        {/* <div className="right-register">
                             <img src="../images/signup.png" alt="" width={200} height={200}/>  
-                        </div> 
+                        </div>  */}
                     </div>
                 </div>
                 
